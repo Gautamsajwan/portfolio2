3cde79cf98d14c0478c8f85e7c3ff641c1d65ca4
gautam <gautamsajwan8126@gmail.com>
2023-11-07 00:48:01 +0530

    enabled images

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -42,7 +42,7 @@ export default function Skill({ directionLeft, imageUrl, proficiency, name }: Pr
         <div className="w-[25px] h-[7px] absolute bg-gray-400 left-0 bottom-0"></div>
         <div className="w-[8px] h-[25px] absolute bg-gray-400 left-0 bottom-0"></div>
 
-        {/* <img className="w-full h-full object-contain p-2 group-hover:blur-[1.5px] group-hover:brightness-[0.7] transition-all duration-200 ease-in-out" src={imageUrl} alt="skill img" /> */}
+        <img className="w-full h-full object-contain p-2 group-hover:blur-[1.5px] group-hover:brightness-[0.7] transition-all duration-200 ease-in-out" src={imageUrl} alt="skill img" />
       </div>
       
       <div className="w-[98px] h-[98px] rounded-lg absolute flex flex-col justify-center items-center text-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold opacity-0 group-hover:opacity-100 group-hover:backdrop-brightness-50 transition-all duration-200 ease-in-out">
